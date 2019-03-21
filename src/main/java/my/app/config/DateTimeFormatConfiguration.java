a7c2c26aa5da005d0eefc612fcd034120b7596d4
Benjamin Billet <contact@benjaminbillet.fr>
2019-03-21 12:56:59 +0100

    Configured new date/time API for Jackson, enforced ISO dates

---
+++ src/main/java/my/app/config/DateTimeFormatConfiguration.java
@@ -0,0 +1,18 @@
+package my.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// this configuration ensure that we will use ISO format for date serialization
+@Configuration
+public class DateTimeFormatConfiguration implements WebMvcConfigurer {
+
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+    DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+    registrar.setUseIsoFormat(true);
+    registrar.registerFormatters(registry);
+  }
+}
---
+++ src/main/java/my/app/config/JacksonConfiguration.java
@@ -0,0 +1,21 @@
+package my.app.config;
+
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// Jackson support for the new Java time API
+@Configuration
+public class JacksonConfiguration {
+  @Bean
+  public JavaTimeModule javaTimeModule() {
+    return new JavaTimeModule();
+  }
+
+  @Bean
+  public Jdk8Module jdk8TimeModule() {
+    return new Jdk8Module();
+  }
+}

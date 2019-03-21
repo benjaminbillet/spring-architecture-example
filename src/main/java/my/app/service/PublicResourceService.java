1d92653f6bd3f62cc5a466fd9219bf0968604dc7
Benjamin Billet <contact@benjaminbillet.fr>
2019-03-21 19:23:08 +0100

    Implemented a minimal service

---
+++ src/main/java/my/app/service/PublicResourceService.java
@@ -0,0 +1,59 @@
+package my.app.service;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import my.app.domain.PublicResource;
+import my.app.dto.PublicResourceDto;
+import my.app.dto.PublicResourceMapper;
+import my.app.repository.PublicResourceRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class PublicResourceService {
+
+  private final Logger log = LoggerFactory.getLogger(PublicResourceService.class);
+
+  private final PublicResourceRepository resourceRepository;
+
+  private final PublicResourceMapper resourceMapper;
+
+  public PublicResourceService(PublicResourceRepository resourceRepository, PublicResourceMapper resourceMapper) {
+    this.resourceRepository = resourceRepository;
+    this.resourceMapper = resourceMapper;
+  }
+
+
+  public PublicResourceDto save(PublicResourceDto dto) {
+    log.debug("Request to save PublicResource : {}", dto);
+    PublicResource resource = resourceMapper.toEntity(dto);
+    resource = resourceRepository.save(resource);
+    return resourceMapper.toDto(resource);
+  }
+
+  @Transactional(readOnly = true)
+  public Page<PublicResourceDto> findAll(Pageable pageable) {
+    log.debug("Request to get all PublicResources");
+    return resourceRepository.findAll(pageable)
+      .map(resourceMapper::toDto);
+  }
+
+  @Transactional(readOnly = true)
+  public Optional<PublicResourceDto> findOne(Long id) {
+    log.debug("Request to get PublicResource : {}", id);
+    return resourceRepository.findById(id)
+      .map(resourceMapper::toDto);
+  }
+
+  public void delete(Long id) {
+    log.debug("Request to delete PublicResource : {}", id);
+    resourceRepository.deleteById(id);
+  }
+}
